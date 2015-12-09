a14d1655f34cf7df0ed8ab8dc88346d1fa867293
Matt Oswalt <matt@keepingitclassless.net>
2015-12-08 17:49:11 -0800

    This will definitely work. Don't even look at it.
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- megaawesome/core.py
+++ megaawesome/core.py
@@ -1,10 +1,8 @@
-
-
 class MegaAwesome(object):
 
     def time_travel(self):
         return True
-
+#asdofiajdsfoiajsdfoijoiegha0w94ut0a9u40t9aur0t9aj0f9gjsdf09gjs0df9gjs0dfm9g0sdf9gjs0df9gjs0df9j
 
 def run():
     ma = MegaAwesome()
